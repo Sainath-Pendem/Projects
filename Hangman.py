5394e25683537ecf02b34842b94d5dfcadbb6b50
Sainath-Pendem <sainathpendem555@gmail.com>
2024-12-25 06:49:56 +0530

    Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,8 @@
+The Hangman game is a classic word-guessing game that challenges players to identify a hidden word one letter at a time.
+The game is designed to be both educational and entertaining, improving vocabulary and spelling skills.
+Players are given a series of blanks representing the letters in the word, and they must guess letters to fill in the blanks.
+Incorrect guesses lead to the drawing of a "hangman," a stick figure that is incrementally drawn with each wrong guess. 
+
 import random
 list=['sainath','dinesh','karthik','rushil','ganesh','tharun','pavan','ram']
 word=random.choice(list)
@@ -56,7 +61,7 @@ for i in range(len(word)):
    display.append('_')
 print(display)
 game_on=True
-lifes=6
+lifes=-1
 while game_on:
    guess=input('choose a letter:').lower()
    if guess in word:
@@ -65,9 +70,9 @@ while game_on:
             display[i]=guess
       print(display)
    else:
-      lifes-=1
+      lifes+=1
       print(hangs[lifes])
-   if lifes == 0:
+   if lifes == 5:
       game_on=False
       print('YOU LOSE')
    if '_' not in display:
